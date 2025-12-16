a71f87746fe0a0ea32ffcf2e53e110253b3406f8
Munirahenad <muneraenad@hotmail.com>
2025-12-16 02:25:37 -0800

    Create sort.h

---
+++ sort.h
@@ -0,0 +1,37 @@
+#ifndef SORT_H
+#define SORT_H
+
+#include <stddef.h>
+
+/**
+ * struct listint_s - Doubly linked list node
+ *
+ * @n: Integer stored in the node
+ * @prev: Pointer to the previous element of the list
+ * @next: Pointer to the next element of the list
+ *
+ * Description: This structure represents a node in a doubly
+ * linked list of integers.
+ */
+typedef struct listint_s
+{
+	const int n;
+	struct listint_s *prev;
+	struct listint_s *next;
+} listint_t;
+
+/* Print functions */
+void print_array(const int *array, size_t size);
+void print_list(const listint_t *list);
+
+/* Sorting algorithms */
+void bubble_sort(int *array, size_t size);
+void selection_sort(int *array, size_t size);
+void quick_sort(int *array, size_t size);
+void shell_sort(int *array, size_t size);
+
+/* List sorting algorithms */
+void insertion_sort_list(listint_t **list);
+void cocktail_sort_list(listint_t **list);
+
+#endif /* SORT_H */
