dd569cdca72c71643397acad0e60a751026d8466
Hamsa1913 <Hamsa12021@gmail.com>
2025-12-17 10:04:31 +0000

    Add counting sort algorithm

--- 102-counting_sort.c
+++ 102-counting_sort.c
@@ -1,6 +1,11 @@
 #include "sort.h"
 #include <stdlib.h>
 
+static int get_max(const int *array, size_t size);
+static int *build_count(const int *array, size_t size, int max);
+static void fill_output(const int *array, size_t size, int *count, int *out);
+static void copy_back(int *array, const int *out, size_t size);
+
 /**
  * counting_sort - Sorts an array of integers in ascending order
  *                 using the Counting sort algorithm
@@ -9,48 +14,81 @@
  */
 void counting_sort(int *array, size_t size)
 {
-	int *count, *output;
-	int max, i;
+	int max, *count, *out;
 
 	if (array == NULL || size < 2)
 		return;
 
-	max = array[0];
-	for (i = 1; i < (int)size; i++)
-		if (array[i] > max)
-			max = array[i];
-
-	count = malloc(sizeof(int) * (max + 1));
+	max = get_max(array, size);
+	count = build_count(array, size, max);
 	if (count == NULL)
 		return;
 
-	for (i = 0; i <= max; i++)
-		count[i] = 0;
+	print_array(count, (size_t)max + 1);
 
-	for (i = 0; i < (int)size; i++)
-		count[array[i]]++;
-
-	for (i = 1; i <= max; i++)
-		count[i] += count[i - 1];
-
-	print_array(count, max + 1);
-
-	output = malloc(sizeof(int) * size);
-	if (output == NULL)
+	out = malloc(sizeof(int) * size);
+	if (out == NULL)
 	{
 		free(count);
 		return;
 	}
 
-	for (i = (int)size - 1; i >= 0; i--)
-	{
-		output[count[array[i]] - 1] = array[i];
-		count[array[i]]--;
-	}
-
-	for (i = 0; i < (int)size; i++)
-		array[i] = output[i];
+	fill_output(array, size, count, out);
+	copy_back(array, out, size);
 
 	free(count);
-	free(output);
+	free(out);
+}
+
+static int get_max(const int *array, size_t size)
+{
+	size_t i;
+	int max;
+
+	max = array[0];
+	for (i = 1; i < size; i++)
+		if (array[i] > max)
+			max = array[i];
+
+	return (max);
+}
+
+static int *build_count(const int *array, size_t size, int max)
+{
+	int *count;
+	size_t i;
+
+	count = malloc(sizeof(int) * (max + 1));
+	if (count == NULL)
+		return (NULL);
+
+	for (i = 0; i <= (size_t)max; i++)
+		count[i] = 0;
+
+	for (i = 0; i < size; i++)
+		count[array[i]]++;
+
+	for (i = 1; i <= (size_t)max; i++)
+		count[i] += count[i - 1];
+
+	return (count);
+}
+
+static void fill_output(const int *array, size_t size, int *count, int *out)
+{
+	int i;
+
+	for (i = (int)size - 1; i >= 0; i--)
+	{
+		out[count[array[i]] - 1] = array[i];
+		count[array[i]]--;
+	}
+}
+
+static void copy_back(int *array, const int *out, size_t size)
+{
+	size_t i;
+
+	for (i = 0; i < size; i++)
+		array[i] = out[i];
 }
