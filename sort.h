3e1890a2f164721edbda74323a5cf978f7213701
Hamsa1913 <Hamsa12021@gmail.com>
2025-12-17 09:58:33 +0000

    Add counting sort algorithm

--- sort.h
+++ sort.h
@@ -4,34 +4,18 @@
 #include <stddef.h>
 
 /**
- * struct listint_s - Doubly linked list node
- *
- * @n: Integer stored in the node
- * @prev: Pointer to the previous element of the list
- * @next: Pointer to the next element of the list
- *
- * Description: This structure represents a node in a doubly
- * linked list of integers.
+ * print_array - Prints an array of integers
+ * @array: Array to print
+ * @size: Size of the array
  */
-typedef struct listint_s
-{
-	const int n;
-	struct listint_s *prev;
-	struct listint_s *next;
-} listint_t;
-
-/* Print functions */
 void print_array(const int *array, size_t size);
-void print_list(const listint_t *list);
 
-/* Sorting algorithms */
-void bubble_sort(int *array, size_t size);
-void selection_sort(int *array, size_t size);
-void quick_sort(int *array, size_t size);
-void shell_sort(int *array, size_t size);
-
-/* List sorting algorithms */
-void insertion_sort_list(listint_t **list);
-void cocktail_sort_list(listint_t **list);
+/**
+ * counting_sort - Sorts an array of integers in ascending order
+ *                 using the Counting sort algorithm
+ * @array: Array of integers
+ * @size: Size of the array
+ */
+void counting_sort(int *array, size_t size);
 
 #endif /* SORT_H */
