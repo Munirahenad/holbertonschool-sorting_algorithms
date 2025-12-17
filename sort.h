30f1b5ef5e0d9945533015d5db3f012e055904cd
Hamsa1913 <Hamsa12021@gmail.com>
2025-12-17 10:15:13 +0000

    Fix merge_sort prototype in sort.h

--- sort.h
+++ sort.h
@@ -3,18 +3,10 @@
 
 #include <stddef.h>
 
-/**
- * print_array - Prints an array of integers
- * @array: Array to print
- * @size: Number of elements in array
- */
+/* Printing helpers */
 void print_array(const int *array, size_t size);
 
-/**
- * counting_sort - Sorts an array of integers using Counting sort algorithm
- * @array: Array of integers
- * @size: Size of the array
- */
-void counting_sort(int *array, size_t size);
+/* Sorting algorithms */
+void merge_sort(int *array, size_t size);
 
 #endif /* SORT_H */
