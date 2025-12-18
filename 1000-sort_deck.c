5cc593641a3ea81304a6644ad215b8a20ae7b08f
munirahenad <muneraenad@hotmail.com>
2025-12-18 12:13:36 +0000

    Implement sort_deck function

---
+++ 1000-sort_deck.c
@@ -0,0 +1,87 @@
+#include <stdlib.h>
+#include <string.h>
+#include "deck.h"
+
+#define DECK_SIZE 52
+
+/**
+ * card_value_rank - Convert card value string to rank (0..12)
+ * @value: Card value string
+ *
+ * Return: rank where Ace=0 ... King=12
+ */
+static int card_value_rank(const char *value)
+{
+	static const char *values[] = {
+		"Ace", "2", "3", "4", "5", "6", "7",
+		"8", "9", "10", "Jack", "Queen", "King"
+	};
+	int i;
+
+	for (i = 0; i < 13; i++)
+	{
+		if (strcmp(value, values[i]) == 0)
+			return (i);
+	}
+	return (-1);
+}
+
+/**
+ * deck_cmp - Comparator for qsort (by kind then by value)
+ * @a: pointer to deck_node_t*
+ * @b: pointer to deck_node_t*
+ *
+ * Return: negative if a<b, positive if a>b, 0 if equal
+ */
+static int deck_cmp(const void *a, const void *b)
+{
+	const deck_node_t *na = *(const deck_node_t * const *)a;
+	const deck_node_t *nb = *(const deck_node_t * const *)b;
+	int ra, rb;
+
+	if (na->card->kind != nb->card->kind)
+		return ((int)na->card->kind - (int)nb->card->kind);
+
+	ra = card_value_rank(na->card->value);
+	rb = card_value_rank(nb->card->value);
+	return (ra - rb);
+}
+
+/**
+ * sort_deck - Sort a deck of cards (52 nodes) in-place by relinking nodes
+ * @deck: Double pointer to the head of the doubly linked list
+ *
+ * Order:
+ * - From Ace to King
+ * - From Spades to Diamonds
+ */
+void sort_deck(deck_node_t **deck)
+{
+	deck_node_t *cur;
+	deck_node_t *arr[DECK_SIZE];
+	size_t i = 0;
+
+	if (deck == NULL || *deck == NULL)
+		return;
+
+	cur = *deck;
+	while (cur && i < DECK_SIZE)
+	{
+		arr[i++] = cur;
+		cur = cur->next;
+	}
+
+	if (i < DECK_SIZE)
+		return;
+
+	qsort(arr, DECK_SIZE, sizeof(deck_node_t *), deck_cmp);
+
+	for (i = 0; i < DECK_SIZE; i++)
+	{
+		arr[i]->prev = (i == 0) ? NULL : arr[i - 1];
+		arr[i]->next = (i == DECK_SIZE - 1) ? NULL : arr[i + 1];
+	}
+
+	*deck = arr[0];
+}
+
